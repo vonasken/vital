0f0ba0f4e55866ee3c5f37761ac6a715b50eb156
Marc Von Asken <vonasken@gmail.com>
2017-02-21 18:19:09 -0800

    Finishing QR Code generator installation

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -3,5 +3,11 @@ class QrCodesController < ApplicationController
   end
 
   def create
+    @qr = RQRCode::QRCode.new( qr_code_params[:text], size: 4)
+  end
+
+private
+  def qr_code_params
+    params.require(:qr_code).permit(:text)
   end
 end
